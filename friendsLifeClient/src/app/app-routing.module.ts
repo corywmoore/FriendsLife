5e6c6a674be6a878020eb36512f190129e3fa5dc
cory Moore <cory@corys-MacBook-Pro-2.local>
2018-04-12 17:57:19 -0500

    things;

--- friendsLifeClient/src/app/app-routing.module.ts
+++ friendsLifeClient/src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { FriendComponent } from './friend/friend.component';
-import { AdminComponent } from './admin/admin.component';
+import { AdminComponent } from './admin/admin/admin.component';
 import { AvailabilityComponent } from './availability/availability.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'friend', component: FriendComponent },
-  { path: 'admin', component: AdminComponent },
   { path: 'friends-life-management', component: AdminComponent },
   { path: 'availability', component: AvailabilityComponent}
 ];
--- friendsLifeClient/src/app/app.module.ts
+++ friendsLifeClient/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
+import { AdminComponent } from './admin/admin/admin.component';
 import { HomeComponent } from './home/home.component';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FriendComponent } from './friend/friend.component';
 import { AvailabilityComponent } from './availability/availability.component';
 import { CategoriesComponent } from './admin/categories/categories.component';
