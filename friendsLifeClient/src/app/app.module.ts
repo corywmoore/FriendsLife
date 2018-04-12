43f6f3f25158892b57de957599570ee3ec530b4e
Micah Wells <micahp0506@gmail.com>
2018-04-12 14:43:55 -0500

    login component added

--- friendsLifeClient/src/app/app.module.ts
+++ friendsLifeClient/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
