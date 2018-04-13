aba4f40622b6377a9511e3837b52b8d7e2e6342a
Micah Wells <micahp0506@gmail.com>
2018-04-13 11:24:11 -0500

    checking in to build

--- friendsLifeClient/src/app/services/admin/admin.service.ts
+++ friendsLifeClient/src/app/services/admin/admin.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+
 
 @Injectable()
 export class AdminService {
 
   constructor(private http: HttpClient) { }
 
+  getHeaders() {
+    const headers = new HttpHeaders()
+        .set('Authorization','B37C60B6C25DC12E3809D89D85A75E92')
+        .set('Content-Type',  'application/json');
+
+    console.log("headers", headers);
+    return headers;
+  }
 
   getCategories() {
-    this.http.get('https://mighty-hollows-34327.herokuapp.com/fl/category').subscribe(
+    this.http.get('https://mighty-hollows-34327.herokuapp.com/fl/category', {headers: this.getHeaders()}).subscribe(
+      data => {
+        console.log(data);
+      });
+  }
+
+  logIn(userId, pw) {
+    this.http.get(`https://mighty-hollows-34327.herokuapp.com/login?loginId=${userId}&password=${pw}`, {}).subscribe(
       data => {
         console.log(data);
       });
