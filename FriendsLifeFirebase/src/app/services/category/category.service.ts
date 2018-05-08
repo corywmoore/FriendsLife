ee25095c1d9e2e5c1b0adbc9fc30a00b0a419a55
cory Moore <cory@corys-MacBook-Pro-2.local>
2018-05-08 14:17:47 -0500

    Now can add image to category. Put in check to redirect from login if have credentials

--- FriendsLifeFirebase/src/app/services/category/category.service.ts
+++ FriendsLifeFirebase/src/app/services/category/category.service.ts
@@ -2,21 +2,24 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore'
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { Upload } from '../../models/upload/upload';
+import * as firebase from 'firebase';
 
 @Injectable()
 
 export class CategoryService {
   categoriesCollection: AngularFirestoreCollection<any> = this.afs.collection('categories');
   categories: Observable<any[]>;
+  basePath = 'uploads/categories';
 
-  constructor(private afs : AngularFirestore) { }
+  constructor(private afs: AngularFirestore) { }
 
   getCategories(cb) {
     this.categories = this.categoriesCollection.snapshotChanges().map(actions => {
-      return actions.map(a=> {
+      return actions.map(a => {
         const data = a.payload.doc.data();
         const id = a.payload.doc.id;
-        return {id, ...data};
+        return { id, ...data };
       });
     });
 
@@ -38,17 +41,58 @@ export class CategoryService {
     this.categoriesCollection.doc(category.id).update({
       "name": category.name,
       "description": category.description,
-      "activities": (category.activities.length > 0) ? category.activities : []
+      "activities": (category.activities.length > 0) ? category.activities : [],
+      "imageUrl": category.imageUrl
     });
+  }
+
+  addImage(upload: Upload, category) {
+    try {
+      console.log(0);
+      console.log('upload', upload);
+      const storageRef = firebase.storage().ref();
+      console.log(0.1);
+      const uploadTask = storageRef.child(`${this.basePath}/${upload.file.name}`).put(upload.file);
+      console.log(0.2);
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => {
+          const snap = snapshot;
+          console.log(1);
+        }, (error) => {
+          console.log(2);
+
+          console.log(error);
+        }, () => {
+          //success
+          console.log(3);
+
+          if (uploadTask.snapshot.downloadURL) {
+            console.log(4);
+
+            category.imageUrl = uploadTask.snapshot.downloadURL;
+            this.updateCategory(category);
+          }
+        }
+      )
+
+    } catch (err) {
+      console.log(100);
+      console.log(err);
+    }
+  }
+
+  deleteImage(category) {
+    category.imageUrl = '';
+    this.updateCategory(category);
   }
 
   getActivityCategories(cb) {
     let tempArray = [];
-    this.categoriesCollection.ref.get().then((qs)=> {
-      qs.forEach((doc)=> {
+    this.categoriesCollection.ref.get().then((qs) => {
+      qs.forEach((doc) => {
         const data = doc.data();
         const id = doc.id;
-        tempArray.push({id, ...data});
+        tempArray.push({ id, ...data });
       });
 
       cb(tempArray);
--- FriendsLifeFirebase/src/environments/environment.ts
+++ FriendsLifeFirebase/src/environments/environment.ts
@@ -10,8 +10,7 @@ export const environment = {
     authDomain: 'friendslife-b3cda.firebaseapp.com',
     databaseURL: 'https://friendslife-b3cda.firebaseio.com',
     projectId: 'friendslife-b3cda',
-    storageBucket: '',
-    messagingSenderId: '631956229203'
+    storageBucket: "friendslife-b3cda.appspot.com",    messagingSenderId: '631956229203'
   }
 };
 
