c25b79bd492c2e875ac362ac2c7fe94181c4cf8b
cory Moore <cory@corys-MacBook-Pro-2.local>
2018-06-13 12:33:56 -0500

    have page wired to one object...now need to get numbering working right

--- FriendsLifeFirebase/src/app/activities/activities.component.ts
+++ FriendsLifeFirebase/src/app/activities/activities.component.ts
@@ -9,23 +9,13 @@ import { CategoryService } from '../services/category/category.service';
   styleUrls: ['./activities.component.scss']
 })
 export class ActivitiesComponent implements OnInit {
-  public mornActivities: ActivityModel[] = [];
-  public aftActivities: ActivityModel[] = [];
-  public currentDay = null;
-  public nextDay = null;
-  public mornRank = 1;
-  public mornSkill: string;
-  public aftRank = 1;
-  public aftSkill: string;
+  public currentDay: string = null;
+  public nextDay = false;
   public warning = false;
   public selection = localStorage.getItem('selectionId');
   public selectedCategories;
-  public currentCategories = new FilteredCategories();
-  public showMorning: boolean = false;
-  public showAfternoon: boolean = false;
 
   private catIndex = 0;
-
   private colors = ['green-activity', 'yellow-activity', 'red-activity'];
 
   constructor(
@@ -35,47 +25,33 @@ export class ActivitiesComponent implements OnInit {
     private categoryService: CategoryService
   ) {
     this.categoryService.getSelectedCategories(this.selection).subscribe((data) => {
-      if (data.length > 0) {
-        console.log('data', data[0]);
-        this.selectedCategories = data[0];
-        this.currentCategories = this.filterCategories(0);
-        console.log('current', this.currentCategories);
-        this.mornActivities = this.filterMornActivities(0);
-        this.aftActivities = this.filterAftActivities(0);
-
-        this.currentDay = this.selectedCategories.categories[this.catIndex].day;
-        this.nextDay = (this.selectedCategories.categories[this.catIndex + 1]) ? this.selectedCategories.categories[1].day : null;
-
-        console.log(this.currentCategories);
-
-        if (this.currentCategories.mornCategories.categories.length > 0) {
-          this.showMorning = true;
-        } else if (this.currentCategories.aftCategories.categories.length > 0) {
-          this.showAfternoon = true;
-        }
-      }
+      console.log('resp', data[0]);
+      this.selectedCategories = this.orderDays(JSON.parse(JSON.stringify(data[0])));
+      console.log('cats', this.selectedCategories);
+      this.currentDay = this.selectedCategories.categories[this.catIndex].day;
+      this.checkThings();
     });
   }
 
   ngOnInit() { }
 
   public submitActivities() {
-
     this.router.navigate(['selections']);
+  }
 
+  public nextActivity() {
+    this.catIndex++;
   }
 
   public resetActivities() {
-    this.mornRank = 1;
-    this.aftRank = 1;
     this.warning = false;
 
-    if (this.currentCategories.mornCategories && this.currentCategories.mornCategories.categories.length > 0) {
-      this.clearCategoryActivitiesRank(this.currentCategories.mornCategories.categories);
+    if (this.selectedCategories.categories[this.catIndex].mornCategories && this.selectedCategories.categories[this.catIndex].mornCategories.categories.length > 0) {
+      this.clearCategoryActivitiesRank(this.selectedCategories.categories[this.catIndex].mornCategories.categories);
     }
 
-    if (this.currentCategories.aftCategories && this.currentCategories.aftCategories.categories.length > 0) {
-      this.clearCategoryActivitiesRank(this.currentCategories.aftCategories.categories);
+    if (this.selectedCategories.categories[this.catIndex].aftCategories && this.selectedCategories.categories[this.catIndex].aftCategories.categories.length > 0) {
+      this.clearCategoryActivitiesRank(this.selectedCategories.categories[this.catIndex].aftCategories.categories);
     }
   }
 
@@ -89,62 +65,59 @@ export class ActivitiesComponent implements OnInit {
     this.categoryService.updateSelectedCategories(this.selection, this.selectedCategories);
   }
 
-  public activityClicked(act: any, i: number, j: number, isMorning: boolean) {
-    console.log('clicked', act);
-    if (act && (!act.rank || (act.rank && act.rank < 1))) {
-      let cat = (isMorning) ? this.currentCategories.mornCategories.categories[i] : this.currentCategories.aftCategories.categories[i];
-      cat.activities[j].rank = (isMorning) ? this.mornRank : this.aftRank;
-      if (isMorning) {
-        this.mornRank++;
-      } else {
-        this.aftRank++;
-      }
-      this.warning = false;
+  public activityClicked(cat: any, act: any) {
+    const rank = this.getRank(cat);
+
+    if (rank < 4) {
+      act.rank = rank;
       this.categoryService.updateSelectedCategories(this.selection, this.selectedCategories);
     }
+
+    this.warning = false;
   }
 
-  public skillClicked(skill: string, isMorning: boolean) {
+  public skillClicked(skill: string, isMorning: boolean, aft: any) {
     let cat = this.selectedCategories.categories[this.catIndex];
-    if (isMorning) {
-      cat.mornCategories.skillLevel = skill;
-      this.currentCategories.mornCategories.skillLevel = skill;
-    } else {
-      cat.aftCategories.skillLevel = skill;
-      this.currentCategories.aftCategories.skillLevel = skill;
+    aft.skillLevel = skill;
+    this.categoryService.updateSelectedCategories(this.selection, this.selectedCategories);
+  }
+
+  private checkThings() {
+    this.nextDay = (this.selectedCategories.categories.length > this.catIndex + 1);
+  }
+
+  private getRank(cat: any) {
+    let rank = 1;
+
+    for (let act of cat.activities) {
+      if (act.rank && act.rank >= rank) {
+        if (act.rank === 3) {
+          return 4;
+        } else {
+          rank = act.rank++;
+        }
+      }
     }
 
-    console.log('updated', this.selectedCategories);
-    console.log('again', this.currentCategories);
-    this.categoryService.updateSelectedCategories(this.selection, this.selectedCategories);
-
-    console.log('skill clicked');
+    return rank;
   }
 
-  public filterCategories(i: number) {
-    let currentCategories = {
-      mornCategories: new SelectionCategories(),
-      aftCategories: new SelectionCategories()
-    };
+  private orderDays(data) {
+    data.categories.sort((a, b) => {
+      return this.sorter[a.day.toLowerCase()] - this.sorter[b.day.toLowerCase()];
+    });
 
-    currentCategories.aftCategories.categories = this.selectedCategories.categories[i].aftCategories.categories.filter((mc) => { return mc.selected });
-    currentCategories.aftCategories.skillLevel = (this.selectedCategories.categories[i].aftCategories.skillLevel) ? this.selectedCategories.categories[i].aftCategories.skillLevel : null;
-    currentCategories.mornCategories.categories = this.selectedCategories.categories[i].mornCategories.categories.filter((mc) => { return mc.selected });
-    currentCategories.mornCategories.skillLevel = (this.selectedCategories.categories[i].mornCategories.skillLevel) ? this.selectedCategories.categories[i].mornCategories.skillLevel : null;
-
-    return currentCategories;
+    return data;
   }
 
-  public filterMornActivities(i: number) {
-    return (this.currentCategories.mornCategories.categories && this.currentCategories.mornCategories.categories.length > 0)
-      ? this.currentCategories.mornCategories.categories[i].activities
-      : null;
-  }
-
-  public filterAftActivities(i: number) {
-    return (this.currentCategories.aftCategories.categories && this.currentCategories.aftCategories.categories.length > 0)
-      ? this.currentCategories.aftCategories.categories[i].activities
-      : null;
+  private sorter = {
+    "sunday": 0,
+    "monday": 1,
+    "tuesday": 2,
+    "wednesday": 3,
+    "thursday": 4,
+    "friday": 5,
+    "saturday": 6
   }
 }
 
