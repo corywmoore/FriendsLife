aa6acc4ecc185ec2b9e054fbd5421d6453c02fb6
cory Moore <cory@corys-MacBook-Pro-2.local>
2018-06-21 17:58:53 -0500

    gets list of classes for dropdown

--- FriendsLifeFirebase/src/app/admin/export/export.component.ts
+++ FriendsLifeFirebase/src/app/admin/export/export.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from '@firebase/util';
+import { ClassService } from '../../services/class/class.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-export',
@@ -8,13 +10,30 @@ import { Observable } from '@firebase/util';
   styleUrls: ['./export.component.scss']
 })
 export class AdminExportComponent implements OnInit {
+  public classes;
+  public exportDataForm: FormGroup;
+  public selectedClass;
+
   selections: any;
   reportsRef: AngularFirestoreCollection<any>;
   reports: any;
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(
+    private afs: AngularFirestore,
+    private svcClass: ClassService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.svcClass.getClasses((data) => {
+      this.classes = data;
+    });
+
+    this.exportDataForm = this.formBuilder.group({
+      id: null,
+      selectedClass: '',
+    });
+
     this.selections = this.afs.collection('selections').valueChanges();
     this.reportsRef = this.afs.collection('reports');
 
